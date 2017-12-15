cd06b9aadeb13c9b694f8fc1cb43d90b1e8943dd
Maheen Khan <maheenak@umich.edu>
2017-12-15 01:00:14 -0500

    Created visualizatons for report

--- 206_project4.py
+++ 206_project4.py
@@ -3,7 +3,12 @@ import facebook
 import json
 import sqlite3
 import sys
-from instagram.client import InstagramAPI
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+plotly.tools.set_credentials_file(username = 'maheenak', api_key ='rWSKmckdQDGohXjqnGSB')
+mapbox_access_token = 'pk.eyJ1IjoibWFoZWVuYWtiYXIiLCJhIjoiY2piN2hpMndvNDBkZzJxbzl0Nzh3enFtYiJ9.MvI-Gs0gmg5f72zYvdKUEA'
 
 token = 'EAAOJEiXVpEMBAJJsR44JOWyJH6LRJZAiYqnZA7TqZAXKBZBHQZAvVqb8xYEFBS5QpWc9tzeY0oeseZBSxJYH5DSsD8II2pfZCdMUM0YXUbUp0m46DbyYZCA08Siibvf9TRpYGpUsaZBl8SyfaV6hQMq9Qdvpui6F8CBcZD'
 
@@ -19,7 +24,7 @@ try:
 except:
     CACHE_DICTION = {}
 
-month_dict = {'01': 'January', '02': 'February', '03': 'March', '04': 'April', '05': 'May', '06': 'June', '07': 'July', '08': 'August', '09': 'September',
+month_dict = {'1': 'January', '2': 'February', '3': 'March', '4': 'April', '5': 'May', '6': 'June', '7': 'July', '8': 'August', '9': 'September',
               '10': 'October', '11': 'November', '12': 'December'}
 latitudes = []
 longitudes = []
@@ -39,7 +44,7 @@ def get_events(key_word):
 
     return results['data']
 
-def get_connections(key_word):
+'''def get_connections(key_word):
 
     word = 'connections: ' + key_word
 
@@ -52,14 +57,14 @@ def get_connections(key_word):
         f.write(json.dumps(CACHE_DICTION))
         f.close
 
-    return results['data']
+    return results['data']'''
 
 party_data = get_events('party')
 celebration_data = get_events('celebartion')
 blowout_data = get_events('blowout')
 festivity_data = get_events('festivity')
 shindig_data = get_events('shindig')
-my_posts = get_connections('posts')
+#my_posts = get_connections('posts')
 
 party_months = {}
 celebration_months = {}
@@ -97,15 +102,21 @@ cur.execute('CREATE TABLE Shindig (name TEXT, date_time DATETIME, state TEXT, co
 def add_data(event_item, dict_input):
     insert_list = [event_item['name'], event_item['start_time']]
 
-
-
     date = event_item['start_time'].split('-')
-    month = date[1]
+    if date[1][0] == '0':
+        month = date[1][1]
+    else:
+        month = date[1]
 
-    if month_dict[month] in dict_input:
+    if month in dict_input:
+        dict_input[month] += 1
+    else:
+        dict_input[month] = 1
+
+    '''if month_dict[month] in dict_input:
         dict_input[month_dict[month]] += 1
     else:
-        dict_input[month_dict[month]] = 1
+        dict_input[month_dict[month]] = 1'''
 
     if 'place' in event_item:
         if 'location' in event_item['place']:
@@ -219,15 +230,93 @@ def create_total():
 
     return total
 
-total_sorted_months = sorted(create_total().items(), key = lambda x: x[1], reverse = True)
+total_months = create_total()
+sorted_total_months = sorted(total_months.items(), key = lambda x: x[1], reverse = True)
 
-print (sorted_party_months)
-print (sorted_celebration_months)
-print (sorted_blowout_months)
-print (sorted_festivity_months)
-print (sorted_shindig_months)
-print (total_sorted_months)
-print (latitudes)
-print (longitudes)
-print (len(latitudes))
-print (len(longitudes))
+
+def create_bar_graph():
+
+    x_axis = []
+    y_axis = []
+    index = 1
+    while index < 13:
+        to_append = str(index)
+        x_axis.append(month_dict[to_append])
+        y_axis.append(total_months[to_append])
+        index += 1
+
+    graph = go.Bar(
+        x = x_axis,
+        y = y_axis,
+    )
+
+    layout = go.Layout(
+        title = 'Event Occurrences with \'Party\' Like Term in Title',
+        xaxis = dict(
+            title = 'Number of Occurrences'
+            ),
+        yaxis = dict(
+            title = 'Month'
+        )
+    )
+    data = [graph]
+    fig = go.Figure(data = data, layout = layout)
+    py.plot(fig, filename = 'total_occurrences_graph')
+
+def create_map():
+
+    data = [
+        go.Scattermapbox(
+            lat = latitudes,
+            lon = longitudes,
+            mode = 'markers',
+
+        )
+    ]
+
+    layout = go.Layout(
+        autosize=True,
+        hovermode='closest',
+        mapbox = dict(
+            accesstoken=mapbox_access_token,
+            bearing = 0,
+            center=dict(
+                lat=38.92,
+                lon=-77.07
+            ),
+            pitch=0,
+            zoom=10
+        ),
+    )
+
+    fig = dict(data = data, layout=layout)
+    py.plot(fig, filename='Map of Event Locations')
+
+
+#create_bar_graph()
+create_map()
+
+
+def print_sorted_tuples(tup_input):
+    for tup in tup_input:
+        month = str(tup[0])
+        print ('{}: {}'.format(month_dict[month], tup[1]))
+    print ('\n')
+
+print ('\'Party\' event occurrences sorted: ')
+print_sorted_tuples(sorted_party_months)
+
+print ('\'Celebration\' event occurrences sorted: ')
+print_sorted_tuples(sorted_celebration_months)
+
+print ('\'Blowout\' event occurrences sorted: ')
+print_sorted_tuples(sorted_blowout_months)
+
+print ('\'Festivity\' event occurrences sorted: ')
+print_sorted_tuples(sorted_festivity_months)
+
+print ('\'Shindig\' event occurrences sorted: ')
+print_sorted_tuples(sorted_shindig_months)
+
+print ('Total event occurrences sorted: ')
+print_sorted_tuples(sorted_total_months)
